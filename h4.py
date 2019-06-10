0744307f395e07966e28011e0f3e1dbb1fef10f2
Wenyi Yan <wyywenyi@gmail.com>
2019-06-09 20:44:58 -0700

    Update h4.py

--- h4.py
+++ h4.py
@@ -1,13 +1,14 @@
 # -*- coding: UTF-8 -*-
 
-import time
-import warnings 
-warnings.filterwarnings("ignore")
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from PIL import Image
-import pytesseract
+#import time
+#import warnings 
+#warnings.filterwarnings("ignore")
+#from selenium import webdriver
+#from selenium.webdriver.common.action_chains import ActionChains
+#from PIL import Image
+#import pytesseract
 
-browser = webdriver.Chrome("./chromedriver")
+#browser = webdriver.Chrome("./chromedriver")
 url = "https://egov.uscis.gov/casestatus/landing.do"
-browser.get(url)
+#browser.get(url)
+print url
